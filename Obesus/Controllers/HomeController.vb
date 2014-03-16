1e8fd69f12e68f3ac504a1c10af5adda49bff3e8
Luis Carlos <lctc91@gmail.com>
2014-03-15 23:03:33 -0600

    inicio

---
+++ Obesus/Controllers/HomeController.vb
@@ -0,0 +1,21 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        ViewData("Message") = "Obesus"
+
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "Acerca de..."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "contactanos"
+
+        Return View()
+    End Function
+End Class
